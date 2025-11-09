e73f349f83ccb8f0d48e6dc1dbd118d2392b50d4
Ismail M <muhammedismailkt@gmail.com>
2025-11-09 14:52:22 +0530

    after complete setup

---
+++ netflix/src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import Footer from '../../Components/footer/footer'
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
